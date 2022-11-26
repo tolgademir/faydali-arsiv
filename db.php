105f3d3e67c262ea3d9c51e08d5038bfae2dda91
Tolga Demir <40199261+tolgademir@users.noreply.github.com>
2022-11-26 13:46:17 +0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$host 		= "localhost";
+$dbname 	= "";
+$charset 	= "utf8";
+$root 		= "";
+$password 	= "";
+
+try {
+    
+  $db = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset;", $root, $password);
+
+} catch(PDOExeption $error) {
+
+  echo $error->getMessage();
+
+}
