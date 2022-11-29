894e762a2e5723c805e73bf05a499c8272d768e0
Tolga Demir <40199261+tolgademir@users.noreply.github.com>
2022-11-29 11:05:07 +0300

    Create cpanel.php

---
+++ cpanel.php
@@ -0,0 +1,8 @@
+<?php
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://myip.cpanel.net/v1.0/');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$addr = curl_exec($curl);
+curl_close($curl);
+header('Location: https://verify.cpanel.net/app/verify?ip='.$addr, true, 303);
+exit;
