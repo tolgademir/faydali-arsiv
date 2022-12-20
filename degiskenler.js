07c5e4bf83d44abdc97a7de88823223e5ea9155e
Tolga Demir <40199261+tolgademir@users.noreply.github.com>
2022-12-20 09:50:49 +0300

    Create degiskenler.js

---
+++ degiskenler.js
@@ -0,0 +1,44 @@
+console.error("Değişkenler");
+
+
+console.warn("var");
+// var
+
+var a = 5;
+var b = 5;
+var c = a + b;
+
+console.log(a,b,c);
+
+console.log(c);
+
+// var ile tanımlanan değişkene tekrardan bir değer tanımlayabiliyoruz.
+
+var a = "hello";
+
+console.log(a); 
+
+
+console.warn("let");
+// let
+
+let x = 10;
+let y = 20;
+let z = x + y;
+
+console.log(x,y,z);
+
+console.log(z);
+
+
+console.warn("Const");
+// const
+
+const sayi1 = 100;
+const sayi2 = 500;
+const toplam = sayi1 + sayi2;
+
+console.log(sayi1,sayi2,toplam);
+
+console.log(toplam);
+
