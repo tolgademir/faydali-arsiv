9f9c59a3d352defd8777f32e2aedfce50e4e5691
Tolga Demir <40199261+tolgademir@users.noreply.github.com>
2023-01-02 20:07:40 +0300

    Create css.php.php

---
+++ css.php.php
@@ -0,0 +1,13 @@
+<?php 
+
+header('Content-type: text/css; charset:UTF-8');
+
+$navbar_arka_plan_renk="black"
+
+?>
+
+.sub-header {
+	background-color: <?= $navbar_arka_plan_renk ?>;
+	height: 46px;
+	line-height: 46px;
+}
